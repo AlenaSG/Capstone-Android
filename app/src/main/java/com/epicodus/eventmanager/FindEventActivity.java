c6cfc1df4566be0ac57b7e23f365395f637918d5
Alena Golovina <alenasgolovina@gmail.com>
2018-01-17 14:14:47 -0800

    add datePicker Dialogue

--- app/src/main/java/com/epicodus/eventmanager/FindEventActivity.java
+++ app/src/main/java/com/epicodus/eventmanager/FindEventActivity.java
@@ -1,11 +1,17 @@
 package com.epicodus.eventmanager;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class FindEventActivity extends AppCompatActivity {
@@ -13,8 +19,9 @@ public class FindEventActivity extends AppCompatActivity {
     private static final String TAG = "FindEventActivity";
 
     private Button btnGoToCalendar;
-    private Button btnChooseDate;
+    private Button btnSelectDate;
     private TextView theDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +30,41 @@ public class FindEventActivity extends AppCompatActivity {
 
         theDate = (TextView) findViewById(R.id.tvDate);
         btnGoToCalendar = (Button) findViewById(R.id.btnGoToCalendar);
+        btnSelectDate = (Button) findViewById(R.id.btnSelectDate);
 
         Intent incomingIntent = getIntent();
         String date = incomingIntent.getStringExtra("date");
         theDate.setText(date);
+
+        btnSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int theYear = cal.get(Calendar.YEAR);
+                int theMonth = cal.get(Calendar.MONTH);
+                int theDay = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        FindEventActivity.this,
+                        android.R.style.Theme_Material_Dialog_NoActionBar_MinWidth,
+                        mDateSetListener,
+                        theYear,theMonth,theDay);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int theYear, int theMonth, int theDay) {
+                theMonth = theMonth + 1;
+
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + theMonth + "/" + theDay + theYear);
+
+                String datePicked = theMonth + "/" + theDay + "/" + theYear;
+                theDate.setText(datePicked);
+            }
+        };
 
         btnGoToCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +73,7 @@ public class FindEventActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
